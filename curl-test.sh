a2ce14b627e9d2edd9f21344be98f99bece9554e
enrique <erivera7240@gmail.com>
2022-06-24 14:56:53 -0500

    adding script for activity 2

---
+++ curl-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=Drip rique&email=erivera7240@gmail.com&content=boiiiiiiiiiiiii'
+curl http://localhost:5000/api/timeline_post
